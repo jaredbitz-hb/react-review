3028a95fa731bed538da98990191cf5afafb8e6a
Jared Bitz <jared.bitz@hackbrightacademy.com>
2023-03-10 09:50:09 -0800

    improved useeffect example

--- static/react_with_fetch.jsx
+++ static/react_with_fetch.jsx
@@ -11,9 +11,13 @@ function ClickButton() {
         });
     }, []);
 
-    const onClick = (event) => {
-        setClicks(clicks + 1);
-        
+    React.useEffect(() => {
+        // Clicks will be 0 when the component is first created, so don't
+        // send that to the server
+        if (clicks == 0) { 
+            return; 
+        }
+
         // Whenever we click the button, also send a POST request to
         // the server to let it know we've clicked again
         fetch('/api/set_clicks', {
@@ -21,9 +25,12 @@ function ClickButton() {
             headers: {
                 'content-type': 'application/json',
             },
-            body: JSON.stringify({'clicks': clicks + 1}),
-        }).then(response => response.json())
-        .then(result => {console.log(result);});
+            body: JSON.stringify({'clicks': clicks}),
+        });
+    }, [clicks]);
+
+    const onClick = (event) => {
+        setClicks(clicks + 1);
     }
 
     return (
